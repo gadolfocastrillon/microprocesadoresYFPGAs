86b492e8c32acbd5d38f95d42180a8745ec3f54b
gadolfocastrillon <gadolfo.castrillon@udea.edu.co>
2023-03-08 19:57:16 -0500

    Actualización del alu

--- alu.vhd
+++ alu.vhd
@@ -32,53 +32,47 @@ BEGIN
 	if selector = "0001" then
 		--Suma de las entradas 
 		salida <= entrada1 + entrada2; 
-	else if selector = "0010" then 
+	elsif selector = "0010" then 
 		--Resta de las entradas
 		salida <= entrada1 - entrada2; 
-		if salida(7) = 1 then 
-			control(2) = 1; 
-		
-	else if selector = "0011" then
+		if salida(7) = '1' then 
+			control(2) = '1'; 
+		else: 
+			salida(7) = '0'
+		end if; 
+	elsif selector = "0011" then
 		--Incremento de A  
-		for i in 0 to 7 loop: 
-			salida(i) <= entrada1(i) + 1; 
-	else if selector = "0100" then 
+		salida<= entrada1 + 1; 
+	elsif selector = "0100" then 
 		--Decremento de A 
-		for i in 0 to 7 loop:
-			salida(i) <= entrada(i) - 1; 
-	else if selector = "0101" then 
+		salida<= entrada - 1; 
+	elsif selector = "0101" then 
 		--Desplazamiento a la derecha 
-		salida(0) <= 0; 
+		salida(0) <= '0'; 
 		for i in 0 to 6 loop: 
 			salida(i+1) <= entrada1(i);  
-	else if selector = "0110" then 
+	elsif selector = "0110" then 
 		--Desplazamiento a la izquierda 
-		salida(7) <=0; 
+		salida(7) <='0'; 
 		for i in 6 to 0 loop: 
 			salida(i+1) <= entrada1(i); 
-	else if selector = "0111" then
+	elsif selector = "0111" then
 		--A and B 
 		salida <= entrada1 and entrada2;  
-	else if selector = "1000" then 
+	elsif selector = "1000" then 
 		--A or B 
 		salida <= entrada1 or entrada2; 
-	else if selector = "1001" then 
+	elsif selector = "1001" then 
 		--A xor B
 		salida <= entrada1 xor entrada2; 
-	else if selector = "1010" then 
+	elsif selector = "1010" then 
 		-- Negación de A 
 		salida <= not(entrada1); 
-	else if selector = "1011" then 
-		--complemento de A 
-		--Es cambiar los valores de 1 a 0 y cambiar los valores de 0 a 1. 
-		
-		for i in 0 to 7 loop: 
-			if (entrada1[i] = 1): 
-				salida[i] = 0; 
-			else: 
-				salida[i] = 1; 
-	
+	elsif selector = "1011" then 
+		--complemento de A + 1. 
+		salida <=not(entrada1) + 1; 
 	else
 		--reset el programa 
-
+		salida <= '00000000'; 
+	end if; 
 END TypeArchitecture;
